3b094887a7ac7088fc6175f2fb82e551feb0922d
Grzegorz Oleksa <grzesieq92@gmail.com>
2026-02-03 20:32:27 +0100

    feat: replace Table component with ExpensesTable and BudgetSummaryTable in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Table } from "./components/Table";
+import { BudgetSummaryTable } from "./components/BudgetSummaryTable/BudgetSummaryTable";
+import { ExpensesTable } from "./components/ExpensesTable/ExpensesTable";
 
 export default function App() {
   return (
     <div>
       <h1 className="bg-red-500">Budżet domowy</h1>
-      <Table />
+      <ExpensesTable />
+      <BudgetSummaryTable />
     </div>
   );
 }
