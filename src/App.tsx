5f47c2bcb514fdd3aec4d291595d39ddf8a6258a
Grzegorz Oleksa <grzesieq92@gmail.com>
2026-02-05 12:51:43 +0100

    feat: add Form component to App for user input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BudgetSummaryTable } from "./components/BudgetSummaryTable/BudgetSummaryTable";
 import { ExpensesTable } from "./components/ExpensesTable/ExpensesTable";
+import { Form } from "./components/Form/Form";
 
 export default function App() {
   return (
     <div>
       <h1 className="bg-red-500">Budżet domowy</h1>
+      <Form />
       <ExpensesTable />
       <BudgetSummaryTable />
     </div>
