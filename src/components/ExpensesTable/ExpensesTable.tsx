8635b205f2b03087e18c8c4e0b4dbe4c15feb198
Grzegorz Oleksa <grzesieq92@gmail.com>
2026-02-05 12:52:15 +0100

    refactor: restructure ExpensesTable component for improved readability

--- src/components/ExpensesTable/ExpensesTable.tsx
+++ src/components/ExpensesTable/ExpensesTable.tsx
@@ -1,45 +1,43 @@
 import { categories } from "../../data/categories";
 import { months } from "../../data/months";
 
-export const ExpensesTable = () => (
-  <>
-    <table className="border border-black border-collapse text-xs">
-      <thead>
-        <tr>
-          <th className="w-40 border border-black">Kategoria</th>
-          {months.map((month) => (
-            <th className="p-2 border border-black bg-blue-100" key={month}>
-              {month}
-            </th>
-          ))}
-          <th className="p-2 border border-black bg-blue-100">
-            średnia
-          </th>
-          <th className="p-2 border border-black bg-blue-100">
-            cel
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {categories.map((category) => (
-          <tr key={category}>
-            <th
-              className="border border-black bg-amber-700 leading-tight"
-              scope="row"
-            >
-              {category}
-            </th>
+export const ExpensesTable = () => {
+  return (
+    <>
+      <table className="border border-black border-collapse text-xs">
+        <thead>
+          <tr>
+            <th className="w-40 border border-black">Kategoria</th>
             {months.map((month) => (
-              <td
-                key={month}
-                className="bg-amber-300 w-25 p-5 text-center border border-black"
-              ></td>
+              <th className="p-2 border border-black bg-blue-100" key={month}>
+                {month}
+              </th>
             ))}
-            <td className="w-25 p-2 border border-black bg-blue-100">0</td>
-            <td className="w-25 p-2 border border-black bg-blue-100">0</td>
+            <th className="p-2 border border-black bg-blue-100">średnia</th>
+            <th className="p-2 border border-black bg-blue-100">cel</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </>
-);
+        </thead>
+        <tbody>
+          {categories.map((category) => (
+            <tr key={category}>
+              <th
+                className="border border-black bg-amber-700 leading-tight"
+                scope="row"
+              >
+                {category}
+              </th>
+              {months.map((month) => (
+                <td
+                  key={month}
+                  className="bg-amber-300 w-25 p-5 text-center border border-black"
+                ></td>
+              ))}
+              <td className="w-25 p-2 border border-black bg-blue-100">0</td>
+              <td className="w-25 p-2 border border-black bg-blue-100">0</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
