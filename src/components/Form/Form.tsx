b10d404dbfd1c504f9776ff37bf93ead1dccc258
Grzegorz Oleksa <grzesieq92@gmail.com>
2026-02-06 20:21:53 +0100

    feat: enhance Form component to manage month selection and update expenses structure

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,20 +1,34 @@
 import type { Dispatch, SetStateAction } from "react";
 import { categories } from "../../data/categories";
+import { months } from "../../data/months";
 
 interface FormProps {
   amount: number;
   setAmount: Dispatch<SetStateAction<number>>;
-  setExpenses: Dispatch<SetStateAction<Record<string, number>>>;
+  setExpenses: Dispatch<SetStateAction<Record<string, Record<string, number>>>>;
   category: string;
   setCategory: Dispatch<SetStateAction<string>>;
+  month: string;
+  setMonth: Dispatch<SetStateAction<string>>;
 }
 
-export const Form = ({ amount, setAmount, setExpenses, category, setCategory }: FormProps) => (
+export const Form = ({
+  amount,
+  setAmount,
+  setExpenses,
+  category,
+  setCategory,
+  month,
+  setMonth,
+}: FormProps) => (
   <form
     className="p-10 border border-black m-10"
     onSubmit={(e) => {
       e.preventDefault();
-      setExpenses((prev) => ({ ...prev, [category]: (prev[category] || 0) + amount }));
+      setExpenses((prev) => ({
+        ...prev,
+        [category]: { ...prev[category], [month]: (prev[category]?.[month] || 0) + amount },
+      }));
       setAmount(0);
     }}
   >
@@ -25,11 +39,31 @@ export const Form = ({ amount, setAmount, setExpenses, category, setCategory }: 
       value={amount}
       onChange={(e) => setAmount(Number(e.target.value))}
     />
-    <select className="border border-red-400" name="category" id="category" value={category} onChange={(e) => setCategory(e.target.value)}>
+    <select
+      className="border border-red-400"
+      name="category"
+      id="category"
+      value={category}
+      onChange={(e) => setCategory(e.target.value)}
+    >
       <option value="">-- wybierz kategorię --</option>
       {categories.map((category) => (
         <option key={category} value={category}>
           {category}
+        </option>
+      ))}
+    </select>
+    <select
+      className="border border-red-400"
+      name="month"
+      id="month"
+      value={month}
+      onChange={(e) => setMonth(e.target.value)}
+    >
+      <option value="">-- wybierz miesiąc --</option>
+      {months.map((month) => (
+        <option key={month} value={month}>
+          {month}
         </option>
       ))}
     </select>
