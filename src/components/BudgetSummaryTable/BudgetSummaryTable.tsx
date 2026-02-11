4abf26c8f3c8249298b76fd77308352628f722d9
Grzegorz Oleksa <grzesieq92@gmail.com>
2026-02-11 19:01:39 +0100

    style: changed styles for budgetSummary and Expenses

--- src/components/BudgetSummaryTable/BudgetSummaryTable.tsx
+++ src/components/BudgetSummaryTable/BudgetSummaryTable.tsx
@@ -17,59 +17,68 @@ export const BudgetSummaryTable = ({
     Object.values(incomes).reduce((sum, cat) => sum + (cat[month] || 0), 0);
 
   return (
-    <table className="text-xs border border-black border-collapse mx-auto">
-        <thead>
-    <tr>
-      <th className="w-40 border border-black">Kategoria</th>
-      {months.map((month) => (
-        <th key={month} className="p-2 border border-black bg-blue-100">
-          {month}
-        </th>
-      ))}
-      <th className="p-2 border border-black bg-blue-100">śr</th>
-      <th className="p-2 border border-black bg-blue-100">cel</th>
-    </tr>
-  </thead>
+    <table className="text-xs border border-black border-collapse mx-auto table-fixed">
+      <thead>
+        <tr>
+          <th className="w-40 border border-black">Kategoria</th>
+          {months.map((month) => (
+            <th key={month} className="w-20 p-2 border border-black bg-blue-100">
+              {month}
+            </th>
+          ))}
+          <th className="w-20 p-2 border border-black bg-blue-100">Śr</th>
+          <th className="w-20 p-2 border border-black bg-blue-100">Cel</th>
+        </tr>
+      </thead>
       <tbody>
         {incomeCategories.map((category) => (
           <tr key={category}>
-            <th className="bg-green-700 border w-40" scope="row">
+            <th className="w-40 bg-green-700 border" scope="row">
               {category}
             </th>
             {months.map((month) => (
-              <td key={month} className="bg-green-300 p-2 border text-center">
+              <td
+                key={month}
+                className="w-20 bg-green-300 p-2 border text-center"
+              >
                 {incomes[category]?.[month] || 0}
               </td>
             ))}
-            <td className="w-25 p-2 border bg-blue-100">0</td>
-            <td className="w-25 p-2 border bg-blue-100">0</td>
+            <td className="w-20 p-2 border bg-blue-100 text-center">0</td>
+            <td className="w-20 p-2 border bg-blue-100 text-center">0</td>
           </tr>
         ))}
 
         <tr>
-          <th className="bg-blue-700 border w-40" scope="row">
+          <th className="w-40 bg-blue-700 border" scope="row">
             Łącznie wpływy
           </th>
           {months.map((month) => (
-            <td key={month} className="bg-blue-300 p-2 border text-center">
+            <td
+              key={month}
+              className="w-20 bg-blue-300 p-2 border text-center"
+            >
               {getTotalIncome(month)}
             </td>
           ))}
-          <td className="w-25 p-2 border bg-blue-100">0</td>
-          <td className="w-25 p-2 border bg-blue-100">0</td>
+          <td className="w-20 p-2 border bg-blue-100 text-center">0</td>
+          <td className="w-20 p-2 border bg-blue-100 text-center">0</td>
         </tr>
 
         <tr>
-          <th className="bg-red-700 border w-40" scope="row">
+          <th className="w-40 bg-red-700 border" scope="row">
             Wydatki
           </th>
           {months.map((month) => (
-            <td key={month} className="bg-red-300 p-2 border text-center">
+            <td
+              key={month}
+              className="w-20 bg-red-300 p-2 border text-center"
+            >
               {getTotalExpenses(month)}
             </td>
           ))}
-          <td className="w-25 p-2 border bg-blue-100">0</td>
-          <td className="w-25 p-2 border bg-blue-100">0</td>
+          <td className="w-20 p-2 border bg-blue-100 text-center">0</td>
+          <td className="w-20 p-2 border bg-blue-100 text-center">0</td>
         </tr>
       </tbody>
     </table>
--- src/components/ExpensesTable/ExpensesTable.tsx
+++ src/components/ExpensesTable/ExpensesTable.tsx
@@ -7,35 +7,38 @@ interface ExpensesTableProps {
 
 export const ExpensesTable = ({ expenses }: ExpensesTableProps) => {
   return (
-    <>
-      <table className="border border-black border-collapse text-xs mx-auto">
-        <thead>
-          <tr>
-            <th className="w-40 border border-black">Kategoria</th>
-            {months.map((month) => (
-              <th className="p-2 border border-black bg-blue-100" key={month}>
-                {month}
-              </th>
-            ))}
-            <th className="p-2 border border-black bg-blue-100">średnia</th>
-            <th className="p-2 border border-black bg-blue-100">cel</th>
-          </tr>
-        </thead>
-        <tbody>
-{categories.map((category) => (
+    <table className="border border-black border-collapse text-xs mx-auto table-fixed">
+      <thead>
+        <tr>
+          <th className="w-40 border border-black">Kategoria</th>
+          {months.map((month) => (
+            <th key={month} className="w-20 p-2 border border-black bg-blue-100">
+              {month}
+            </th>
+          ))}
+          <th className="w-20 p-2 border border-black bg-blue-100">Średnia</th>
+          <th className="w-20 p-2 border border-black bg-blue-100">Cel</th>
+        </tr>
+      </thead>
+      <tbody>
+        {categories.map((category) => (
           <tr key={category}>
-            <th className="bg-amber-700 border w-40" scope="row">{category}</th>
+            <th className="w-40 bg-amber-700 border" scope="row">
+              {category}
+            </th>
             {months.map((month) => (
-              <td key={month} className="bg-amber-300 p-2 border text-center">
+              <td
+                key={month}
+                className="w-20 bg-amber-300 p-2 border text-center"
+              >
                 {expenses[category]?.[month] || 0}
               </td>
             ))}
-            <td className="w-25 p-2 border bg-blue-100">0</td>
-            <td className="w-25 p-2 border bg-blue-100">0</td>
+            <td className="w-20 p-2 border bg-blue-100 text-center">0</td>
+            <td className="w-20 p-2 border bg-blue-100 text-center">0</td>
           </tr>
         ))}
-        </tbody>
-      </table>
-    </>
+      </tbody>
+    </table>
   );
 };
