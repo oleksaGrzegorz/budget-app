3bc860f6a20688554e50b92a1a7563ed14daa8d1
Grzegorz Oleksa <grzesieq92@gmail.com>
2026-02-15 21:10:10 +0100

    refactor: remove unused labels

--- src/data/budgetSummaryLabels.ts
+++ src/data/budgetSummaryLabels.ts
@@ -1,11 +1,6 @@
 export const budgetSummaryLabels = [
   "Zarobki Grzesiek",
-  "Zarobki G - inne",
   "Zarobki Marta",
   "Wpływy inne",
-  "Łącznie wpływy",
-  "Wydatki",
-  "Główne wydatki",
-  "Dodatkowe wydatki",
   "Oszczędności",
 ];  
