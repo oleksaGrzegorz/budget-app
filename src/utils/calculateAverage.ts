b238dba2b234088b65bcfc6631b90d6604496265
Grzegorz Oleksa <grzesieq92@gmail.com>
2026-05-20 22:13:01 +0200

    feat: update calculateAverage function to consider values up to the current month

--- src/utils/calculateAverage.ts
+++ src/utils/calculateAverage.ts
@@ -1,7 +1,12 @@
-export const calculateAverage = (values: number[]): number | null => {
-  const filtered = values.filter((v) => v > 0);
-  if (filtered.length === 0) return null;
+export const calculateAverage = (
+  values: number[],
+  currentMonthIndex = new Date().getMonth(), // maj = 4
+): number | null => {
+  const monthsUntilNow = values.slice(0, currentMonthIndex + 1);
 
-  const sum = filtered.reduce((acc, val) => acc + val, 0);
-  return sum / filtered.length;
+  if (monthsUntilNow.length === 0) return null;
+
+  const sum = monthsUntilNow.reduce((acc, val) => acc + (val || 0), 0);
+
+  return sum / monthsUntilNow.length;
 };
