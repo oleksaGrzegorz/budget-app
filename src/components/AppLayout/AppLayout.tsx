346ac5e6fc44d6212aa011ccfdf3a110648be4d8
Grzegorz Oleksa <grzesieq92@gmail.com>
2026-06-18 14:09:13 +0200

    refactor: update AppLayout to use max-w-screen-2xl for improved layout

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -14,7 +14,7 @@ export const AppLayout = ({ theme, children }: AppLayoutProps) => {
           : "bg-slate-300 text-slate-800"
       }`}
     >
-      <main className="mx-auto max-w-7xl space-y-10 px-6 py-10">
+      <main className="mx-auto max-w-screen-2xl space-y-10 px-6 py-10">
         {children}
       </main>
     </div>
