3dab3c4f5997aa6149e1614b499fca7f6dbe5cc7
Grzegorz Oleksa <grzesieq92@gmail.com>
2026-06-21 09:12:54 +0200

    refactor: simplify BudgetInsights component by removing unused cards

--- src/components/BudgetInsights/BudgetInsights.tsx
+++ src/components/BudgetInsights/BudgetInsights.tsx
@@ -2,8 +2,6 @@ import type { Forecast } from "../../data/initialForecast";
 import type { BudgetData } from "../../types/budgetData";
 import type { GoalsData } from "../../types/goalsData";
 import type { PeriodOption } from "../../utils/budgetAverages";
-import { BiggestOverspendingCard } from "./BiggestOverspendingCard";
-import { BiggestSavingsCard } from "./BiggestSavingsCard";
 import { IncomeGoalCard } from "./IncomeGoalCard";
 
 interface BudgetInsightsProps {
@@ -15,31 +13,11 @@ interface BudgetInsightsProps {
 }
 
 export const BudgetInsights = ({
-  expenses,
   incomes,
-  expenseGoals,
   forecast,
   period,
 }: BudgetInsightsProps) => {
   return (
-    <section className="grid gap-4 lg:grid-cols-3">
-      <BiggestSavingsCard
-        expenses={expenses}
-        expenseGoals={expenseGoals}
-        period={period}
-      />
-
-      <IncomeGoalCard
-        incomes={incomes}
-        forecast={forecast}
-        period={period}
-      />
-
-      <BiggestOverspendingCard
-        expenses={expenses}
-        expenseGoals={expenseGoals}
-        period={period}
-      />
-    </section>
+    <IncomeGoalCard incomes={incomes} forecast={forecast} period={period} />
   );
 };
