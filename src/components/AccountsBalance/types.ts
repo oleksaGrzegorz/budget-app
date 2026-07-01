2ab6ade7559298d3cd695e362bdfbd1629cc7df5
Grzegorz Oleksa <grzesieq92@gmail.com>
2026-07-01 20:47:21 +0200

    refactor: update AccountDefinition type to include accountDefinitions

--- src/components/AccountsBalance/types.ts
+++ src/components/AccountsBalance/types.ts
@@ -1,4 +1,7 @@
 import type { AccountId } from "../../data/initialAccountSnapshots";
+import type { accountDefinitions } from "../../data/initialAccountSnapshots";
+
+export type AccountDefinition = (typeof accountDefinitions)[number];
 
 export type ChartMetricId = AccountId | "pln.total" | "eur.total" | "total.eur";
 
