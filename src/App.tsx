1dc7924b9d41d200fb7959aaafe0fe4dd80535ba
Grzegorz Oleksa <grzesieq92@gmail.com>
2026-07-14 22:03:28 +0200

    refactor: move ExpensesList to transactions components

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import { BudgetSummaryTable } from "./components/BudgetSummaryTable/BudgetSummar
 import { BudgetUsageSummary } from "./components/BudgetUsageSummary/BudgetUsageSummary";
 import { ExpensesByCategoryChart } from "./components/ExpensesByCategoryChart/ExpensesByCategoryChart";
 import { ExpensesByIncomeChart } from "./components/ExpensesByIncomeChart/ExpensesByIncomeChart";
-import { ExpensesList } from "./components/ExpensesList/ExpensesList";
 import { ExpensesTable } from "./components/ExpensesTable/ExpensesTable";
 import { IncomeForecastTable } from "./components/IncomeForecastTable/IncomeForecastTable";
 import { IncomesTable } from "./components/IncomesTable/IncomesTable";
@@ -18,6 +17,7 @@ import type { PeriodOption } from "./utils/budgetAverages";
 
 import { initialAccountSnapshots } from "./data/initialAccountSnapshots";
 
+import { ExpensesList } from "./features/transactions/components/ExpensesList/ExpensesList";
 import { Form } from "./features/transactions/components/TransactionForm/Form/Form";
 import { AppLayout } from "./shared/components/AppLayout/AppLayout";
 import { DashboardCard } from "./shared/components/DashboardCard/DashboardCard";
